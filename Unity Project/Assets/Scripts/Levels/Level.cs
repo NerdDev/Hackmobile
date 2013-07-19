9589622e8c18ee217e7241bed7c9a4278f4f098d
Justin Swanson <Leviathan1753@gmail.com>
2013-07-19 17:06:08 -0500

    Level Convenience Functions
    Former-commit-id: deabcfe0063c3d952cb4ae69bd25c2d05c355b85

--- Unity Project/Assets/Scripts/Levels/Level.cs
+++ Unity Project/Assets/Scripts/Levels/Level.cs
@@ -7,4 +7,64 @@ public class Level
     protected LevelLayout Layout;
     public GridSpace[,] Arr { get; private set; }
 
+    public GridSpace Get(int x, int y)
+    {
+        if (x < Arr.GetLength(1) && y < Arr.GetLength(0))
+        {
+            return Arr[y, x];
+        }
+        return null;
+    }
+
+    #region ConvenienceFunctions
+    public void Put(int x, int y, WorldObject obj)
+    {
+        Get(x,y).Put(obj);
+    }
+
+    public void Remove(int x, int y, WorldObject obj)
+    {
+        Get(x, y).Remove(obj);
+    }
+
+    public bool Accept(int x, int y, WorldObject obj)
+    {
+        return Get(x, y).Accept(obj);
+    }
+
+    public bool IsBlocked(int x, int y)
+    {
+        return Get(x, y).IsBlocked();
+    }
+
+    public bool HasNonBlocking(int x, int y)
+    {
+        return Get(x, y).HasNonBlocking();
+    }
+
+    public bool HasObject(int x, int y)
+    {
+        return Get(x, y).HasObject();
+    }
+
+    public bool IsEmpty(int x, int y)
+    {
+        return Get(x, y).IsEmpty();
+    }
+
+    public List<WorldObject> GetContained(int x, int y)
+    {
+        return Get(x, y).GetContained();
+    }
+
+    public List<WorldObject> GetFreeObjects(int x, int y)
+    {
+        return Get(x, y).GetFreeObjects();
+    }
+
+    public List<WorldObject> GetBlockingObjects(int x, int y)
+    {
+        return Get(x, y).GetBlockingObjects();
+    }
+    #endregion
 }
