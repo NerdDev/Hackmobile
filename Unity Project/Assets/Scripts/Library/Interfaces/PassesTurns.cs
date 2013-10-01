1200729abd89eb6f43e10560c7dbe2fff85be87f
Justin Swanson <Leviathan1753@gmail.com>
2013-10-01 16:54:58 -0500

    Cleaning while browsing
    Former-commit-id: 27cabad530c0f0b48e4a53dd9cebbab82e0d6dce

--- Unity Project/Assets/Scripts/Library/Interfaces/PassesTurns.cs
+++ Unity Project/Assets/Scripts/Library/Interfaces/PassesTurns.cs
@@ -1,8 +1,6 @@
-﻿public interface PassesTurns
+public interface PassesTurns
 {
     void UpdateTurn();
-    //public int currentPoints;
-    //public int basePoints;
     int CurrentPoints { get; set; }
     int BasePoints { get; set; }
     bool IsActive { get; set; }
