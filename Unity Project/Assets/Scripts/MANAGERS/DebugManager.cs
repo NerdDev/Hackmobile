dd4488eea2ac37426b40dcd61e15d58b14da58b4
Justin Swanson <Leviathan1753@gmail.com>
2013-12-03 07:28:01 -0600

    More timing debug
    Former-commit-id: 96ae03cc3b8ade62baf3a79cd34f83bbe408ecab

--- Unity Project/Assets/Scripts/MANAGERS/DebugManager.cs
+++ Unity Project/Assets/Scripts/MANAGERS/DebugManager.cs
@@ -34,7 +34,7 @@ public class DebugManager : MonoBehaviour, IManager
     {
         GlobalLogging,
         LineNumbers,
-        SearchSteps,
+        LevelGenFineSteps,
         Probability,
         LevelGen_Path_Simplify_Prune,
         LevelGen_Connected_To,
