8939c62c7762dd8537edb81cbee77c21e2edc950
Leviathan1753 <Leviathan1753@gmail.com>
2014-03-07 18:32:40 -0600

    Tomb Prefab
    Former-commit-id: 937a800b889f830625af52f2604e6c1701d9e13d

--- Unity Project/Assets/Scripts/Levels/Theme/UndeadTombTheme/TombElement.cs
+++ Unity Project/Assets/Scripts/Levels/Theme/UndeadTombTheme/TombElement.cs
@@ -2,8 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using UnityEngine;
 
 public class TombElement : ThemeElement
 {
+    public override List<GenDeploy> PreDeployTweaks(ThemeElementSpec spec)
+    {
+
+    }
 }
 
